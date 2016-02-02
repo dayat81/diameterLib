e9dd8ce50e0cbbe64cbc05ed414efffe71e8a2d9
Hidayat Hidayat <uda.dayat@gmail.com>
2016-02-02 11:31:18 +0700

    Initial Commit

---
+++ diameterLib/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  diameterLib
+//
+//  Created by hidayat on 2/2/16.
+//  Copyright © 2016 hidayat. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
